cc8b6d2cc828920446c36a29da29dd91f137cf0e
javi Conejero <ja.conejero@alumnos.upm.es>
2025-01-26 17:13:27 +0100

    techo finalizado

--- escenas/Galeria.gd
+++ escenas/Galeria.gd
@@ -4,6 +4,7 @@ extends Node3D
 @export var floor_scene: PackedScene = preload("res://escenas/Suelo.tscn")
 @export var wall_scene: PackedScene = preload("res://escenas/pared.tscn")
 @export var wall_scene_no_canva: PackedScene = preload("res://escenas/pared_vacia.tscn")
+@export var Wooden_roof: PackedScene = preload("res://escenas/techoEscena.tscn")
 # Número de elementos
 @export var num_elements: int = ControladorImagenes.size()
 
@@ -17,15 +18,16 @@ func add_elements(n):
 	for i in range(int(n/2)):
 		add_suelo(Vector3(4+(4*i), 0, 2))
 		add_suelo(Vector3(4+(4*i), 0, -2))
+		add_techo(Vector3(2+(4*i), 4, 0))
 		# Crear y posicionar paredes
 		add_bombilla(Vector3(4+(4*i), 2, 0))
 		add_pared_con_cuadro(Vector3(4+(4*i), 2, -4),null,null)
 		add_pared_con_cuadro(Vector3(4+(4*i), 2, 4) ,Vector3(0,PI,0),null)
 		
 	if n%2==0:
-		add_pared_sin_cuadro(Vector3(2+(4*n/2), 2, 0),Vector3(0,3*PI/2,0),Vector3(8,4,4))
+		add_pared_sin_cuadro(Vector3(2+(4*n/2), 2, 0),Vector3(0,3*PI/2,0),Vector3(8,8.5,4))
 	else:
-		add_pared_con_cuadro(Vector3((4*n/2),2, 0),Vector3(0,3*PI/2,0),Vector3(2,1,1))
+		add_pared_con_cuadro(Vector3((4*n/2),2, 0),Vector3(0,3*PI/2,0),Vector3(2,2.5,1))
 
 		
 func add_pared_sin_cuadro(posicion,rotacion,escala):
@@ -52,6 +54,11 @@ func add_suelo(posicion):
 	var instancia_suelo = floor_scene.instantiate()
 	instancia_suelo.position = posicion # Ajusta la posición como necesites
 	add_child(instancia_suelo)
+
+func add_techo(posicion):
+	var instancia = Wooden_roof.instantiate()
+	instancia.position = posicion # Ajusta la posición como necesites
+	add_child(instancia)
 
 func add_bombilla(posicion):
 	var instancia_luz = OmniLight3D.new()
